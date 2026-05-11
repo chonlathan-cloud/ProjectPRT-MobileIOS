30b5746b9aa9fcdc3eb504f38e220d3c13c0ea38
git config --global user.name Chonlathan Songsri <git config --global user.email chonlathan@manee-son.com>
2026-05-11 16:52:21 +0700

    Initial Commit

---
+++ PRTApp_Workspace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PRTApp_Workspace
+//
+//  Created by Chonlathan Songsri on 11/5/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PRTApp_Workspace/PRTApp_WorkspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PRTApp_WorkspaceApp.swift
+//  PRTApp_Workspace
+//
+//  Created by Chonlathan Songsri on 11/5/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct PRTApp_WorkspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
